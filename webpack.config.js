a02f3094ee5fe4b32ef3f74874ed6e50d0bf340d
artem <artempushkar@gmail.com>
2017-08-10 03:17:08 +0300

    add enxtension files

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,11 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
         publicPath: "/assets/"
+    },
+
+    module: {
+        rules: [
+            {test: /\.ts$/, use: 'ts-loader'}
+        ]
     }
 };
